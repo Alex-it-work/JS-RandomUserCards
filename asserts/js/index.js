5669b234f8e7958e0f8d3e1ee8bbe356fba82ca5
Alex-it-work <Mr.Kirieiev@gmail.com>
2021-06-30 22:10:55 +0300

    Add: css animation, btn |< , userd data into ucard

---
+++ asserts/js/index.js
@@ -0,0 +1,102 @@
+"use strict";
+
+// - Добавить кнопку << , т.е. переход на первую страницу.
+// - Добавить информацию о юзере (возраст, имейл, ...).
+// - Цвет рамки (фона) карточкам генерить в зависимости от пола юзера.
+// - * Сдалать возможным выбирать несколько карточек, список полных имен выбранных юзеров приводить в строку сверху. Выбранные карточки подсвечивать.
+// - Застилить карточки.
+
+const options = {
+  results: 10,
+  seed: "abc",
+  page: 1,
+};
+
+loadUsers(options);
+
+const [btnStart, btnPrev, btnNext] = document.querySelectorAll("button");
+btnStart.addEventListener("click", btnStartHandler);
+btnPrev.addEventListener("click", btnPrevHandler);
+btnNext.addEventListener("click", btnNextHandler);
+
+function btnStartHandler(e) {
+  options.page = 1;
+  loadUsers(options);
+}
+
+function btnPrevHandler(e) {
+  if (options.page >= 1) {
+    options.page--;
+    loadUsers(options);
+  }
+}
+
+function btnNextHandler(e) {
+  options.page++;
+  loadUsers(options);
+}
+
+function loadUsers({ results, seed, page }) {
+  fetch(
+    `https://randomuser.me/api/?results=${results}&seed=${seed}&page=${page}`
+  )
+    .then(response => response.json())
+    .then(({ results }) => renderUsers(results));
+}
+
+function renderUsers(users) {
+  const userList = document.querySelector(".userList");
+  if (userList) {
+    userList.remove();
+  }
+  console.dir(users);
+  const newUserList = document.createElement("ul");
+  newUserList.classList.add("userList");
+  document.getElementById("root").prepend(newUserList);
+
+  const liUserCollection = users.map(user => createUserListItem(user));
+  newUserList.append(...liUserCollection);
+}
+
+function createUserListItem({
+  name: { first: firstName, last: lastName },
+  picture: { large: userImageSrc },
+  dob: { age: age },
+  email: email,
+  cell: cell,
+  location: { country: country },
+}) {
+  const userListItem = document.createElement("li");
+  userListItem.classList.add("userListItem");
+
+  userListItem.append(createUserImage(userImageSrc));
+  userListItem.append(createUserFullName(firstName, lastName));
+  userListItem.append(createUserOtherData(age, email, cell, country));
+  // userListItem.append(createUserAge(age));
+  // userListItem.append(createUserEmail(email));
+  // userListItem.append(createUserCell(cell));
+  // userListItem.append(createUserCountry(country));
+
+  return userListItem;
+}
+
+function createUserImage(userImageSrc) {
+  const img = new Image();
+  img.src = userImageSrc;
+  img.alt = "user profile image";
+  return img;
+}
+
+function createUserFullName(firstName, lastName) {
+  const div = document.createElement("div");
+  div.classList.add("userFullName");
+  div.innerText = `${firstName} ${lastName}`;
+  return div;
+}
+
+function createUserOtherData(age, email, cell, country) {
+  const div = document.createElement("div");
+  div.classList.add("userOtherData");
+  div.innerText = `Age: ${age}, email: ${email}, cell: ${cell}, country: ${country}`;
+  return div;
+}
